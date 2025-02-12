5b3320b85aefe6e5333d8b1a61f0b6c0999996e5
LisandrOviedo <lisandroviedo@hotmail.com>
2025-02-12 15:50:51 -0400

    Style: La opacidad se ha cambiado de 50 a 1

--- Client/src/components/UI/Container.jsx
+++ Client/src/components/UI/Container.jsx
@@ -10,7 +10,7 @@ export const Container = ({
   return (
     <div
       className={`flex h-screen pt-[25vh] flex-1 flex-col items-center px-6 lg:px-8 justify-between ${
-        blockEvents && "opacity-50 pointer-events-none"
+        blockEvents && "opacity-1 pointer-events-none"
       } ${className}`}
       {...props}
     >
